022863725f2de9f1e597b171d8769902d26e7ea2
Claus-git <dmscheinberg@gmail.com>
2024-12-06 13:10:13 -0500

    cleaned up code

--- pin-3.31/source/tools/forwardedge/forwardedge.cpp
+++ pin-3.31/source/tools/forwardedge/forwardedge.cpp
@@ -22,10 +22,6 @@ using std::hex;
 // Global variables
 /* ================================================================== */
 
-UINT64 insCount    = 0; //number of dynamically executed instructions
-UINT64 bblCount    = 0; //number of dynamically executed basic blocks
-UINT64 threadCount = 0; //total number of threads, including main thread
-
 ADDRINT offset;
 ADDRINT lowest;
 ADDRINT highest;
@@ -41,46 +37,13 @@ unordered_map<int, bool> addrMap;
 /* ===================================================================== */
 // Command line switches
 /* ===================================================================== */
-KNOB< string > KnobOutputFile(KNOB_MODE_WRITEONCE, "pintool", "o", "", "specify file name for forwardedge output");
 
-KNOB< BOOL > KnobCount(KNOB_MODE_WRITEONCE, "pintool", "count", "1",
-                       "count instructions, basic blocks and threads in the application");
-
-KNOB< string > KnobInputFile(KNOB_MODE_WRITEONCE, "pintool", "i", "", "specify file name for forwardedge input");
-
-/* ===================================================================== */
-// Utilities
-/* ===================================================================== */
-
-/*!
- *  Print out help message.
- */
-INT32 Usage()
-{
-    cerr << "This tool prints out the number of dynamically executed " << endl
-         << "instructions, basic blocks and threads in the application." << endl
-         << endl;
-
-    cerr << KNOB_BASE::StringKnobSummary() << endl;
-
-    return -1;
-}
+KNOB< string > KnobInputFile(KNOB_MODE_WRITEONCE, "pintool", "f", "", "specify file name for forwardedge input");
 
 /* ===================================================================== */
 // Analysis routines
 /* ===================================================================== */
 
-/*!
- * Increase counter of the executed basic blocks and instructions.
- * This function is called for every basic block when it is about to be executed.
- * @param[in]   numInstInBbl    number of instructions in the basic block
- * @note use atomic operations for multi-threaded applications
- */
-VOID CountBbl(UINT32 numInstInBbl)
-{
-    bblCount++;
-    insCount += numInstInBbl;
-}
 
 VOID countIndirect(ADDRINT target) {
     if(loaded) {
@@ -89,44 +52,20 @@ VOID countIndirect(ADDRINT target) {
                 printf("address not allowed %lx absolute addr %lx\n", target-lowest, target);
                 exit(0);
             }
-            *out << hex << target-lowest << endl;
+            // *out << hex << target-lowest << endl;
             indirectCount++;
         }
-    }
-    
+    } 
 }
 
 /* ===================================================================== */
 // Instrumentation callbacks
 /* ===================================================================== */
 
-/*!
- * Insert call to the CountBbl() analysis routine before every basic block 
- * of the trace.
- * This function is called every time a new trace is encountered.
- * @param[in]   trace    trace to be instrumented
- * @param[in]   v        value specified by the tool in the TRACE_AddInstrumentFunction
- *                       function call
- */
-VOID Trace(TRACE trace, VOID* v)
-{
-    // Visit every basic block in the trace
-    for (BBL bbl = TRACE_BblHead(trace); BBL_Valid(bbl); bbl = BBL_Next(bbl))
-    {
-        // Insert a call to CountBbl() before every basic bloc, passing the number of instructions
-        BBL_InsertCall(bbl, IPOINT_BEFORE, (AFUNPTR)CountBbl, IARG_UINT32, BBL_NumIns(bbl), IARG_END);
-    }
-}
-
 VOID Instruction(INS ins, VOID* v) {
     if (INS_IsIndirectControlFlow(ins) && INS_IsCall(ins)) {
         INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)countIndirect, IARG_BRANCH_TARGET_ADDR, IARG_END);
-        // *out << ins << endl;
     }
-}
-
-VOID ApplicationStart(VOID* v) {
-
 }
 
 VOID ImageLoad (IMG img, VOID* V) {
@@ -137,46 +76,12 @@ VOID ImageLoad (IMG img, VOID* V) {
         highest = IMG_HighAddress(img);
         printf("highest: %lx lowest: %lx\n", highest, lowest);
     }
-
-    // for( SYM sym= IMG_RegsymHead(img); SYM_Valid(sym); sym = SYM_Next(sym) ) {
-    //     std::cerr << SYM_Name(sym) << endl;
-    // }
 }
 
 VOID ImageUnload(IMG img, VOID* v) {
     if (IMG_IsMainExecutable(img)) {
         loaded = false;
     }
-}
-
-/*!
- * Increase counter of threads in the application.
- * This function is called for every thread created by the application when it is
- * about to start running (including the root thread).
- * @param[in]   threadIndex     ID assigned by PIN to the new thread
- * @param[in]   ctxt            initial register state for the new thread
- * @param[in]   flags           thread creation flags (OS specific)
- * @param[in]   v               value specified by the tool in the 
- *                              PIN_AddThreadStartFunction function call
- */
-VOID ThreadStart(THREADID threadIndex, CONTEXT* ctxt, INT32 flags, VOID* v) { threadCount++; }
-
-/*!
- * Print out analysis results.
- * This function is called when the application exits.
- * @param[in]   code            exit code of the application
- * @param[in]   v               value specified by the tool in the 
- *                              PIN_AddFiniFunction function call
- */
-VOID Fini(INT32 code, VOID* v)
-{
-    *out << "===============================================" << endl;
-    *out << "MyPinTool analysis results: " << endl;
-    *out << "Number of instructions: " << insCount << endl;
-    *out << "Number of basic blocks: " << bblCount << endl;
-    *out << "Number of threads: " << threadCount << endl;
-    *out << "Number of indirect control flows: " << indirectCount << endl;
-    *out << "===============================================" << endl;
 }
 
 /*!
@@ -190,16 +95,7 @@ int main(int argc, char* argv[])
 {
     // Initialize PIN library. Print help message if -h(elp) is specified
     // in the command line or the command line is invalid
-    if (PIN_Init(argc, argv))
-    {
-        return Usage();
-    }
-
-    PIN_InitSymbols();
-
-    
-
-
+    PIN_Init(argc, argv);
 
     string inFileName = KnobInputFile.Value();
     if (!inFileName.empty()) {
@@ -214,40 +110,15 @@ int main(int argc, char* argv[])
         }
         printf("numaddr: %d\n", numaddr);
     }
-    
 
-    string fileName = KnobOutputFile.Value();
+    INS_AddInstrumentFunction(Instruction, 0);
 
-    if (!fileName.empty())
-    {
-        out = new std::ofstream(fileName.c_str());
-    }
+    IMG_AddInstrumentFunction(ImageLoad, 0);
 
-    if (KnobCount)
-    {
-        // Register function to be called to instrument traces
-        TRACE_AddInstrumentFunction(Trace, 0);
-
-        INS_AddInstrumentFunction(Instruction, 0);
-
-        IMG_AddInstrumentFunction(ImageLoad, 0);
-        IMG_AddUnloadFunction(ImageUnload, 0);
-
-        // Register function to be called for every thread before it starts running
-        PIN_AddThreadStartFunction(ThreadStart, 0);
-
-        PIN_AddApplicationStartFunction(ApplicationStart, 0);
-
-        // Register function to be called when the application exits
-        PIN_AddFiniFunction(Fini, 0);
-    }
+    IMG_AddUnloadFunction(ImageUnload, 0);
 
     cerr << "===============================================" << endl;
-    cerr << "This application is instrumented by MyPinTool" << endl;
-    if (!KnobOutputFile.Value().empty())
-    {
-        cerr << "See file " << KnobOutputFile.Value() << " for analysis results" << endl;
-    }
+    cerr << "This application is instrumented by forwardedge" << endl;
     cerr << "===============================================" << endl;
 
     // Start the program, never returns
